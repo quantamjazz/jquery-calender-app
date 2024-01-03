6ce70f636e599e9c5b9397b0b0f7128f3a591493
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2024-01-03 17:57:51 +0000

    added time blocks for 24 hour clock

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    var currentDate = dayjs().format('DD, MM, YYYY'); // Format the date as you like
+    $('#currentDay').text(currentDate); // Sets the text of the element with id 'currentDay' to the current date
+  });
+  
