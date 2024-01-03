44c15b351b5914ab84c01165ea74ee707c6f33fa
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2024-01-03 19:28:04 +0000

    loop through time blocks to compare hours and added classes for past, present and future hours

--- js/script.js
+++ js/script.js
@@ -2,5 +2,21 @@ $(document).ready(function() {
     var currentDate = dayjs().format('DD, MM, YYYY'); // Format the date as you like
     $('#currentDay').text(currentDate); // Sets the text of the element with id 'currentDay' to the current date
   });
+
+  $(document).ready(function() {
+    var currentHour = dayjs().hour(); // Get current hour as a number
+  
+    $('.time-block').each(function() {
+      var blockHour = parseInt($(this).attr('id').replace('hour-', '')); // Get hour from the block's id
+  
+      if (blockHour < currentHour) {
+        $(this).addClass('past'); // Add 'past' class
+      } else if (blockHour === currentHour) {
+        $(this).addClass('present'); // Add 'present' class
+      } else {
+        $(this).addClass('future'); // Add 'future' class
+      }
+    });
+  });
   
   
