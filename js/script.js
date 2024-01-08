b9dbfd84595398dc0a8f113b7c2b74bc5010e3d2
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2024-01-08 19:29:12 +0000

    adjusted class names of text areas in timeblocks as they were not consistent with JS file which caused local storage errors

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
 $(document).ready(function() {
-  // Set the current date
-  var currentDate = dayjs().format('DD, MM, YYYY');
-  $('#currentDay').text(currentDate);
+    // Set the current date
+    var currentDate = dayjs().format('DD, MM, YYYY');
+    $('#currentDay').text(currentDate);
 
-  // Load events from local storage
-  $('.time-block').each(function() {
-      var eventTime = $(this).attr('id');
-      var eventText = localStorage.getItem(eventTime);
-      if (eventText !== null) {
-          $(this).find('.event-text').val(eventText);
-      }
-  });
+    // Load and color-code events
+    var currentHour = dayjs().hour();
+    $('.time-block').each(function() {
+        var blockHour = parseInt($(this).attr('id').replace('hour-', ''));
+        var eventText = localStorage.getItem($(this).attr('id'));
+        
+        if (eventText) {
+            $(this).find('.event-text').val(eventText);
+        }
 
-  // Color-code time blocks
-  var currentHour = dayjs().hour();
-  $('.time-block').each(function() {
-      var blockHour = parseInt($(this).attr('id').replace('hour-', ''));
-      if (blockHour < currentHour) {
-          $(this).addClass('past'); 
-      } else if (blockHour === currentHour) {
-          $(this).addClass('present'); 
-      } else {
-          $(this).addClass('future'); 
-      }
-  });
+        if (blockHour < currentHour) {
+            $(this).addClass('past');
+        } else if (blockHour === currentHour) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+        }
+    });
 
-  // Add event listener for save buttons here
-  $('.saveBtn').on('click', function() {
-    var eventText = $(this).siblings('.event-text').val(); // Retrieves the event text from the input field
-    var eventTime = $(this).parent().attr('id'); // Retrieves the id of the parent element, which indicates the time block
-    localStorage.setItem(eventTime, eventText); // Saves the event text in local storage with
+    // Save button event listener
+    $('.saveBtn').on('click', function() {
+        var eventText = $(this).siblings('.event-text').val();
+        var eventTime = $(this).parent().attr('id');
+        localStorage.setItem(eventTime, eventText);
+    });
 
+    // Clear button function for clearing content and local storage
+    $("#clearFieldsBtn").click(function(event) {
+        event.preventDefault();
+        $('.event-text').val("");
+        localStorage.clear();
+    });
 });
